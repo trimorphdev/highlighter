f50fc6b0170028fee66823e21f7253c46cb3279c
Zack Pace <trimorphyt@gmail.com>
2022-08-28 08:34:49 -0700

    feat: add Brainheck language support

--- packages/highlighter/src/lib.rs
+++ packages/highlighter/src/lib.rs
@@ -2,3 +2,9 @@
 
 pub use highlighter_core as core;
 pub use highlighter_core::{highlight, Token};
+
+pub use highlighter_languages as languages;
+pub use highlighter_languages::highlight_language;
+
+#[cfg(feature = "brainheck")]
+pub use highlighter_languages::brainheck::Brainheck;
