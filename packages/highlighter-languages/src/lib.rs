88e0cc972a7f94ab61b117d5e4ba492711951c73
Zack Pace <trimorphyt@gmail.com>
2022-08-28 08:58:12 -0700

    feat: add `language` method to easily find a language.

--- packages/highlighter-languages/src/lib.rs
+++ packages/highlighter-languages/src/lib.rs
@@ -1,9 +1,19 @@
 //! Official language extensions for Highlighter.
 
-use highlighter_core::{Token, Error, highlight};
+use brainheck::Brainheck;
+use highlighter_core::{Token, Error, highlight, language::Language};
 
 #[cfg(feature = "brainheck")]
 pub mod brainheck;
+
+/// Returns the language with the specified name, if any.
+pub fn language(name: &str) -> Option<Box<dyn Language>> {
+    match name.to_lowercase().as_str() {
+        #[cfg(feature = "brainheck")]
+        "brainheck" => Some(Box::new(Brainheck)),
+        _ => None
+    }
+}
 
 /// Highlights a language extended from the `highlighter-languages` crate.
 pub fn highlight_language(language: &str, src: &str) -> Option<Result<Vec<Token>, Error>> {
--- packages/highlighter/src/lib.rs
+++ packages/highlighter/src/lib.rs
@@ -4,10 +4,11 @@ pub use highlighter_core as core;
 pub use highlighter_core::{highlight, Token};
 
 pub use highlighter_languages as languages;
-pub use highlighter_languages::highlight_language;
+pub use highlighter_languages::{highlight_language, language};
 
 #[cfg(feature = "brainheck")]
 pub use highlighter_languages::brainheck::Brainheck;
 
 #[cfg(feature = "target-html")]
 pub use highlighter_target_html::HighlighterTargetHtml;
+
